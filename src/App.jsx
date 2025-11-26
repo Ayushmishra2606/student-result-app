957fe565b741fc56a2a05d659eaa9e0bdc426da6
Ayush <imayush2702@gmail.com>
2025-11-26 23:33:22 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -17,22 +17,34 @@ export default function App() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-6">Student Result Management</h1>
+  <div className="h-screen bg-gray-100 p-6 overflow-hidden">
+    <h1 className="text-3xl font-bold text-center mb-6">
+      Student Result Management
+    </h1>
 
-      <div className="max-w-3xl mx-auto">
+    
+    <div className="flex gap-6 h-[85vh]">
+
+     
+      <div className="w-1/3 mt-4">
         <StudentForm
           fetchStudents={fetchStudents}
           editingStudent={editingStudent}
           setEditingStudent={setEditingStudent}
         />
+      </div>
 
+      
+      <div className="w-2/3 overflow-y-auto rounded">
         <StudentList
           students={students}
           fetchStudents={fetchStudents}
           setEditingStudent={setEditingStudent}
         />
       </div>
+      
     </div>
-  );
+  </div>
+);
+
 }
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -6,6 +6,22 @@ export default function StudentList({ students, fetchStudents, setEditingStudent
     fetchStudents();
   };
 
+  
+  const getGrade = (marks) => {
+    if (marks >= 90) return "O";
+    if (marks >= 80) return "A+";
+    if (marks >= 70) return "A";
+    if (marks >= 60) return "B+";
+    if (marks >= 50) return "B";
+    if (marks >= 40) return "C";
+    return "F";
+  };
+
+ 
+  const getStatus = (marks) => {
+    return marks < 27 ? "Fail" : "Pass";
+  };
+
   return (
     <div className="p-4 bg-white rounded shadow mt-4">
       <h2 className="text-xl font-semibold mb-4">Student List</h2>
@@ -17,34 +33,53 @@ export default function StudentList({ students, fetchStudents, setEditingStudent
             <th>Roll</th>
             <th>Section</th>
             <th>Marks</th>
-            
+            <th>Status</th>
+            <th>Grade</th>
           </tr>
         </thead>
 
         <tbody>
-          {students.map((s) => (
-            <tr key={s.id} className="border-b">
-              <td>{s.name}</td>
-              <td>{s.roll}</td>
-              <td>{s.section}</td>
-              <td>{s.result}</td>
-              <td className="space-x-2">
-                <button
-                  onClick={() => setEditingStudent(s)}
-                  className="px-2 py-1 bg-yellow-400 rounded m-1"
-                >
-                  Edit
-                </button>
+          {students.map((s) => {
+            const status = getStatus(s.result);
+            const grade = getGrade(s.result);
 
-                <button
-                  onClick={() => handleDelete(s.id)}
-                  className="px-2 py-1 bg-red-500 text-white rounded m-1"
+            return (
+              <tr key={s.id} className="border-b">
+                <td>{s.name}</td>
+                <td>{s.roll}</td>
+                <td>{s.section}</td>
+                <td>{s.result}</td>
+
+               
+                <td
+                  className={
+                    status === "Pass" ? "text-green-600 font-semibold" : "text-red-600 font-semibold"
+                  }
                 >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
+                  {status}
+                </td>
+
+              
+                <td className="font-semibold">{grade}</td>
+
+                <td className="space-x-2">
+                  <button
+                    onClick={() => setEditingStudent(s)}
+                    className="px-2 py-1 bg-yellow-400 m-1.5 rounded"
+                  >
+                    Edit
+                  </button>
+
+                  <button
+                    onClick={() => handleDelete(s.id)}
+                    className="px-2 py-1 bg-red-500 text-white rounded m-1.5"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
